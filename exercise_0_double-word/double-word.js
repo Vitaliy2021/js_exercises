767e253e38fdfc90415c9b981425e542daf3d61e
Vitaliy Ivanov <vitaliy@mack.local>
2021-09-17 15:21:15 +0200

    added exersice_0

---
+++ exercise_0_double-word/double-word.js
@@ -0,0 +1,9 @@
+// exercise 0: napisz funkcje, ktora przyjmuje stringa i zwraca ten
+//  string, powtorzony dwa razy ze spacja pomiedzy nimi.
+// naprzyklad:  ab -> ab ab
+// orzech -> orzech orzech
+
+function doubleWord(a) {
+    return a+" "+a
+}
+console.log(doubleWord("orzech"))
